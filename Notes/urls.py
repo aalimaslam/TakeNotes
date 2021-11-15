7d5d7c41d95d17e4e632238c3bfe6f66b99e1a9c
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2021-11-15 13:11:49 +0530

    Update urls.py

--- Notes/urls.py
+++ Notes/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from TakeNotes import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name = "Home"),
@@ -29,4 +31,5 @@ urlpatterns = [
     path('changepassword',views.ChangePassword,name = "Update"),
 
 
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
