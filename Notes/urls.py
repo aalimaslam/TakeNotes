12325a6cb19455eb74b0e1d896257df19201df4b
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2021-11-15 13:16:13 +0530

    Update urls.py

--- Notes/urls.py
+++ Notes/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from TakeNotes import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name = "Home"),
@@ -29,6 +32,8 @@ urlpatterns = [
     path('read',views.see,name = "profile"),
     path('update',views.update,name = "Update"),
     path('changepassword',views.ChangePassword,name = "Update"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
